13f77862d8b02d12d371c12ee8d291e801b39724
AlexPetrusca <petruscalex@gmail.com>
2026-03-03 11:26:40 -0800

    add h2 database

--- backend/src/main/java/com/comethru/mocker/ComethruMockerApplication.java
+++ backend/src/main/java/com/comethru/mocker/ComethruMockerApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.comethru.mocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ComethruMockerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ComethruMockerApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/comethru/mocker/ComethruMockerTests.java
+++ backend/src/test/java/com/comethru/mocker/ComethruMockerTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.comethru.mocker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ComethruMockerTests {
 
 	@Test
 	void contextLoads() {
