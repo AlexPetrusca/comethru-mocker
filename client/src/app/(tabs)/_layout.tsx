1a04e069ed2b48af6b4ba4e89d820bc8708a5d75
AlexPetrusca <petruscalex@gmail.com>
2026-03-05 14:08:24 -0800

    remove modal

--- client/src/app/(tabs)/_layout.tsx
+++ client/src/app/(tabs)/_layout.tsx
@@ -27,21 +27,7 @@ export default function TabLayout() {
               tintColor={color}
               size={28}
             />
-          ),
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable style={{ marginRight: 15 }}>
-                {({ pressed }) => (
-                  <SymbolView
-                    name={{ ios: 'info.circle', android: 'info', web: 'info' }}
-                    size={25}
-                    tintColor={Colors[colorScheme].text}
-                    style={{ opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          )
         }}
       />
       <Tabs.Screen
