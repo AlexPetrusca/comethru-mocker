6e18d313ed82a038923bbf0f6c1409a2fbeefbb9
AlexPetrusca <petruscalex@gmail.com>
2026-03-13 20:29:11 -0700

    On physical devices, connect to my laptop :)

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import { Platform } from "react-native";
+import * as Device from 'expo-device';
 
-const API_PORT = 8090;
-const API_BASE_URL = Platform.OS === "android"
-  ? `http://10.0.2.2:${API_PORT}`
-  : `http://localhost:${API_PORT}`;
+const API_BASE_URL = Device.isDevice
+  ? `http://10.0.0.232:8099`
+  : Platform.OS === "android"
+    ? `http://10.0.2.2:8090`
+    : `http://localhost:8090`;
+
+console.log(Device);
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
